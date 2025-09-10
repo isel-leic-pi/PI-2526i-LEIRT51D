bad8fe6d0ab9605ebe90caba73867377228297d1
fbfreitas <ffreitas@cc.isel.ipl.pt>
2025-09-10 10:54:31 +0100

    aula 2

---
+++ aula2/intro.js
@@ -0,0 +1,38 @@
+
+console.log("HELLO WORLD")
+
+let a = "STR"
+
+console.log(a)
+console.log(typeof(a))
+
+a = 1223
+
+console.log(a)
+console.log(typeof(a))
+
+a = true
+console.log(a)
+console.log(typeof(a))
+
+let b
+console.log(b)
+console.log(typeof(b))
+
+b = null
+console.log(b)
+console.log(typeof(b))
+
+console.log("31" + 2)
+console.log("31" - 2)
+console.log(typeof("31" - 2))
+
+const e = b || -2 || 10
+
+console.log(e)
+
+const k = "21"
+const z = 21
+
+console.log(k == z)
+console.log(k === z)
---
+++ aula2/objects.js
@@ -0,0 +1,37 @@
+const std = {name: "Filipe", number : 12123}
+
+console.log(std)
+
+std.email = "fil@cc.isel.pt"
+console.log(std)
+console.log(std.email)
+
+delete std.email
+console.log(std)
+console.log(std.email)
+
+console.log(std.number)
+console.log(std["number"])
+
+function createStudent(name, number){
+    const st = {}
+    st.name=name
+    st.number=number
+    return st
+
+}
+
+const std2 = createStudent("joao", 12334)
+console.log(std2)
+
+function showProps(obj)
+{
+    for(let p in obj)
+    {
+        console.log(p + " : " + obj[p])
+    }
+}
+
+showProps(std2)
+
+
